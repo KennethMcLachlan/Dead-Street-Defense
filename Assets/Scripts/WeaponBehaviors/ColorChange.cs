1b7718529ce93e788df70f6733e565b78e799b75
Kenneth McLachlan <121731402+KennethMcLachlan@users.noreply.github.com>
2025-06-12 17:36:29 -0700

    Prototype Color Change Sscript completed

--- Assets/Scripts/WeaponBehaviors/ColorChange.cs
+++ Assets/Scripts/WeaponBehaviors/ColorChange.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class ColorChange : MonoBehaviour
 {
     private Renderer[] _renderers;
-    private Material[] _originalMaterials;
+    private Material[][] _originalMaterials;
 
     private GameObject _muzzleFlash;
+
+    [SerializeField] private float _transparencyStrength = 0.4f;
 
     void Start()
     {
@@ -13,12 +15,25 @@ public class ColorChange : MonoBehaviour
         _renderers = GetComponentsInChildren<Renderer>();
 
         // Cache original materials so we can reset later
-        _originalMaterials = new Material[_renderers.Length];
+        _originalMaterials = new Material[_renderers.Length][];
+
         for (int i = 0; i < _renderers.Length; i++)
         {
-            // Use material to create instance unique to this object
-            _renderers[i].material = new Material(_renderers[i].material);
-            _originalMaterials[i] = _renderers[i].material;
+            Material[] originalMats = _renderers[i].materials;
+            _originalMaterials[i] = new Material[originalMats.Length];
+
+            Material[] instanceMats = new Material[originalMats.Length];
+            for (int j = 0; j < originalMats.Length; j++)
+            {
+                instanceMats[j] = new Material(originalMats[j]);
+                _originalMaterials[i][j] = new Material(originalMats[j]);
+            }
+
+            _renderers[i].materials = instanceMats;
+
+            //// Use material to create instance unique to this object
+            //_renderers[i].material = new Material(_renderers[i].material);
+            //_originalMaterials[i] = _renderers[i].material;
         }
 
         _muzzleFlash = transform.Find("Muzzle_Flash")?.gameObject;
@@ -47,56 +62,107 @@ public class ColorChange : MonoBehaviour
     {
         DisableMuzzleFlash();
 
-        foreach (var mat in _originalMaterials)
+        foreach (var renderer in _renderers)
         {
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", Color.red);
-            Color color = mat.color;
-            color.a = 0.1f;
-            mat.color = color;
-        }
+            foreach (var mat in renderer.materials)
+            {
+                if (mat.HasProperty("_EmissionColor"))
+                {
+                    mat.EnableKeyword("_EMISSION");
+                    mat.SetColor("_EmissionColor", Color.red);
+                }
 
+                SetMaterialToTransparent(mat);
+
+                if (mat.HasProperty("_Color"))
+                {
+                    Color color = mat.color;
+                    color.a = _transparencyStrength;
+                    mat.color = color;
+                }
+            }
+
+        }
     }
 
     public void TurnBlue()
     {
         DisableMuzzleFlash();
-
-        foreach (var mat in _originalMaterials)
+        foreach (var renderer in _renderers)
         {
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", Color.cyan);
-            Color color = mat.color;
-            color.a = 0.1f;
-            mat.color = color;
-        }
+            foreach (var mat in renderer.materials)
+            {
+                if (mat.HasProperty("_EmissionColor"))
+                {
+                    mat.EnableKeyword("_EMISSION");
+                    mat.SetColor("_EmissionColor", Color.cyan);
+                }
 
+                SetMaterialToTransparent(mat);
+
+                if (mat.HasProperty("_Color"))
+                {
+                    Color color = mat.color;
+                    color.a = _transparencyStrength;
+                    mat.color = color;
+                }
+            }
+        }
     }
 
     public void TurnGreen()
     {
         DisableMuzzleFlash();
 
-        foreach (var mat in _originalMaterials)
+        foreach (var renderer in _renderers)
         {
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", Color.green);
-            Color color = mat.color;
-            color.a = 0.1f;
-            mat.color = color;
-        }
+            foreach (var mat in renderer.materials)
+            {
+                if (mat.HasProperty("_EmissionColor"))
+                {
+                    mat.EnableKeyword("_EMISSION");
+                    mat.SetColor("_EmissionColor", Color.green);
+                }
 
+                SetMaterialToTransparent(mat);
+
+                if (mat.HasProperty("_Color"))
+                {
+                    Color color = mat.color;
+                    color.a = _transparencyStrength;
+                    mat.color = color;
+                }
+            }
+        }
     }
 
     public void ResetToOriginal()
     {
-        foreach (var renderer in _renderers)
+        for (int i = 0; i < _renderers.Length; i++)
         {
-            // Restore original shared material to fully reset appearance
-            renderer.material = renderer.sharedMaterial;
+            Material[] restored = new Material[_originalMaterials[i].Length];
+            for (int j= 0; j < restored.Length; j++)
+            {
+                restored[j] = new Material(_originalMaterials[i][j]);
+            }
+            _renderers[i].materials = restored;
         }
 
         EnableMuzzleFlash();
+    }
+
+    private void SetMaterialToTransparent(Material mat)
+    {
+        if (mat == null) return;
+
+        mat.SetFloat("_Mode", 3);
+        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+        mat.SetInt("_ZWrite", 0);
+        mat.DisableKeyword("_ALPHATEST_ON");
+        mat.EnableKeyword("_ALPHABLEND_ON");
+        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+        mat.renderQueue = (int)UnityEngine.Rendering.RenderQueue.Transparent;
     }
 
     private void EnableMuzzleFlash()
@@ -116,69 +182,5 @@ public class ColorChange : MonoBehaviour
     }
 }
 
-
-
-//    using UnityEngine;
-
-//public class ColorChange : MonoBehaviour
-//{
-//    private MeshRenderer[] _renderers;
-//    private Material[] _materials;
-
-//    void Start()
-//    {
-//        _renderers = GetComponentsInChildren<MeshRenderer>();
-//        _materials = new Material[_renderers.Length];
-
-//        for (int i = 0; i < _renderers.Length; i++)
-//        {
-//            // Use `.material` to get an instance per object (not sharedMaterial)
-//            _materials[i] = _renderers[i].material;
-//        }
-//    }
-
-//    private void Update()
-//    {
-//        if (Input.GetKeyDown(KeyCode.R)) TurnRed();
-//        if (Input.GetKeyDown(KeyCode.B)) TurnBlue();
-//        if (Input.GetKeyDown(KeyCode.G)) TurnGreen();
-//        if (Input.GetKeyDown(KeyCode.L)) FullColor();
-//    }
-
-//    public void TurnRed()
-//    {
-//        ApplyColor(Color.red, 0.1f);
-//    }
-
-//    public void TurnBlue()
-//    {
-//        ApplyColor(Color.cyan, 0.1f);
-//    }
-
-//    public void TurnGreen()
-//    {
-//        ApplyColor(Color.green, 0.1f);
-//    }
-
-//    public void FullColor()
-//    {
-//        ApplyColor(Color.white, 1.0f);
-//    }
-
-//    private void ApplyColor(Color emissionColor, float alpha)
-//    {
-//        foreach (var mat in _materials)
-//        {
-//            if (mat == null) continue;
-
-//            mat.EnableKeyword("_EMISSION");
-//            mat.SetColor("_EmissionColor", emissionColor);
-
-//            Color baseColor = mat.color;
-//            baseColor.a = alpha;
-//            mat.color = baseColor;
-//        }
-//    }
-//}
 
 
