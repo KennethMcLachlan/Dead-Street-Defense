99e359129756ff1380fb3961559e98f83c92b340
Kenneth McLachlan <121731402+KennethMcLachlan@users.noreply.github.com>
2026-03-12 19:50:48 -0700

    Swapped enemy model for a zombie model

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -3,15 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    private void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.Escape))
-        //{
-        //    //UIManager.Instance.EngagePauseMenu();
-        //    PlaybackHandler.Instance.PauseGame();
-        //}
-    }
-
     public void RestartGame()
     {
         Time.timeScale = 1.0f;
