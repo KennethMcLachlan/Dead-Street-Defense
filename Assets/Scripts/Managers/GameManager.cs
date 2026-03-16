f0d185b663e386cbe3403fd7360493bf34e47a49
KennethMcLachlan <kennethmclachlan11@gmail.com>
2026-03-16 16:51:15 -0700

    Added application Version number sync to pint current cersion on Player HUD

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Text _versionText;
+
+    private void Start()
+    {
+        _versionText.text = $"v{Application.version}";
+    }
+
     public void RestartGame()
     {
         Time.timeScale = 1.0f;
